159c20cf6edf285a41983839410cad137f0aafa9
People-11 <349504120p@gmail.com>
2024-07-29 16:19:57 +1000

    Update

--- clash.py
+++ clash.py
@@ -1,23 +1,27 @@
 import requests
 
-url = "https://raw.githubusercontent.com/ignaciocastro/a-dove-is-dumb/main/clash.txt"
+url = "https://raw.githubusercontent.com/ignaciocastro/a-dove-is-dumb/main/clash.yaml"
 response = requests.get(url)
 content = response.text
 
-with open('clash.list', 'w') as file:
+with open('clash.yaml', 'w') as file:
     file.write(content)
 
 lines = content.splitlines()
 comments = []
 domains = []
 
+in_payload_section = False
+
 for line in lines:
     if line.startswith('#'):
         comments.append(line)
-    elif line.startswith('DOMAIN'):
-        domains.append(f"  - {line}")
+    elif line.strip() == 'payload:':
+        in_payload_section = True
+    elif in_payload_section and line.strip().startswith('- DOMAIN,'):
+        domains.append(line.strip()[2:].strip())
 
-output = '\n'.join(comments) + '\n' + 'payload:\n' + '\n'.join(domains)
+output = '\n'.join(comments) + '\n' + '\n'.join(domains)
 
-with open('clash.yaml', 'w') as file:
+with open('clash.list', 'w') as file:
     file.write(output)
